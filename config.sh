f029f3ec39f779b95eaeaf14cfb4995aa01883b7
Mateusz Korzeniewski <themrp8@gmail.com>
2018-11-20 21:08:26 +0100

    Add WIP version of BuildSuccer 3.0. I'm uploading this to prevent further borks, so somebody who knows bash a little more then me can safely check it and see if everything is alright.

---
+++ config.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+ 
+# BuildSuccer example config file
+
+# Manifest configuration
+BS_MANIFEST_REPO="https://github.com/LineageOS/android.git"
+BS_MANIFEST_BRANCH="cm-14.1"
+
+# Using local manifests from
+BS_LOCAL_MANIFEST_USE=true
+
+# Set this to true to get local manifest from Git repo
+BS_LOCAL_MANIFEST_GIT=true
+BS_LOCAL_MANIFEST_REPO="https://github.com/mattroot/local_manifests.git"
+# Local manifest branch
+BS_LOCAL_MANIFEST_BRANCH="cm-14.1-sirius"
+
+# Use this to copy over local manifest
+BS_LOCAL_MANIFEST_CP=false
+BS_LOCAL_MANIFEST_PATH="~/local_manifests/sirius.xml"
+
+# Build configuration
+BS_BUILD_TARGET="lineage_sirius-userdebug"
+# Make command used for starting build
+BS_BUILD_MAKE="bacon"
+# Set this to true to use brunch instead - useful for Lineage ROMs
+BS_BUILD_BRUNCH=true
+
+# Telegram deploy module configuration
+BS_DEPLOY_API_TOKEN=
+
+# Information used in release messages
+BS_DEPLOY_DISPLAY_NAME="Lineage"
+BS_DEPLOY_DISPLAY_VERSION="14.1"
+BS_DEPLOY_DISPLAY_MAINTAINER=$(whoami)
+BS_DEPLOY_DISPLAY_DEVICE="Xperia Z2 (sirius)"
+
+# CI channel configuration - broadcasts all messages related to build - job start and end time, etc.
+BS_DEPLOY_CI_USE=true
+BS_DEPLOY_CI_CHANNEL=
+
+# Releases channel configuration - only broadcasts finished builds 
+BS_DEPLOY_RELEASE_USE=true
+BS_DEPLOY_RELEASE_CHANNEL=
+
+# PRE DEPLOY TASKS
+# Before deploying a build to Telegram, you need some variables.
+# These variables are required to push the build to Pixeldrain (defining ex. file name) and fill in the release post.
+# This function is called after the build, right before Pixeldrain upload and Telegram deployment.
+BS_DEPLOY_PREDEPLOY_TASKS() {
+
+}
